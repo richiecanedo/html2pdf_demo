2ba5e6b4c17fed1553521ee3e10c0c4251671839
richiecanedo <richiecanedo@gmail.com>
2017-10-08 16:23:01 -0700

    Primer commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+use Spiru\Html2Pdf\Html2pdf;
+
+if(isset($_POST['crear'])){
+    //Recoger el contenido del otro fichero
+    ob_start();
+    require_once 'print_view.php';
+    $html = ob_get_clean();
+    
+    $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+    $html2pdf->writeHTML($html);
+    $html2pdf->output('pdf_generated.pdf');
+}
+?>
+
+<form action="" method="POST">
+    <input type="text" placeholder="Titulo" name="titulo" />
+    <input type="submit" value="Crear un pdf" name="crear" />
+</form>
+
